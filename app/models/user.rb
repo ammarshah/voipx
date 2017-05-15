6326331f973394068d84c1373c82199072d34c48
Ammar Shah <ammarshah123@gmail.com>
2017-05-15 16:54:33 +0500

    Assign role accordingly after user creation

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,4 +12,20 @@ class User < ApplicationRecord
   belongs_to :company, optional: true
 
   accepts_nested_attributes_for :company, reject_if: :all_blank
+
+  after_create :assign_role
+
+  private
+  def is_a_company_admin?
+    return false if company.nil?
+    company.users.count == 1 ? true : false
+  end
+
+  def assign_role
+    if is_a_company_admin?
+      add_role(:company_admin)
+    else
+      add_role(:user)
+    end
+  end
 end
