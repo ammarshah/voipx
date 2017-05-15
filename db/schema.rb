3b191db803374826f045b0af338c60e9ff92782a
Ammar Shah <ammarshah123@gmail.com>
2017-05-15 13:10:04 +0500

    Add Company model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170511114220) do
+ActiveRecord::Schema.define(version: 20170515080856) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "companies", force: :cascade do |t|
+    t.string   "name"
+    t.string   "location"
+    t.string   "website"
+    t.string   "phone_no"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "roles", force: :cascade do |t|
     t.string   "name"
---
+++ spec/factories/companies.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :company do
+    name "MyString"
+    location "MyString"
+    website "MyString"
+    phone_no "MyString"
+  end
+end
