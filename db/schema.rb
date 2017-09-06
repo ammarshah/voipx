add190c587a4ac91dfe821b4d150afb0ea14fbf7
Ammar Shah <ammarshah123@gmail.com>
2017-09-06 12:26:26 +0500

    Add index for destination in breakouts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170721063049) do
+ActiveRecord::Schema.define(version: 20170803123659) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20170721063049) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.index ["code"], name: "index_breakouts_on_code", unique: true, using: :btree
+    t.index ["destination"], name: "index_breakouts_on_destination", using: :btree
   end
 
   create_table "buying_countries", id: false, force: :cascade do |t|
