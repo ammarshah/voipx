5e4472663066a7d9a0552401bb430952531897a7
Ammar Shah <ammarshah123@gmail.com>
2018-01-16 10:49:57 +0500

    Favorites page new design

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -2,6 +2,6 @@ class ContactsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @contacts = current_user.contacts
+    @contacts = current_user.contacts.includes(:user).order(created_at: :desc)
   end
 end
