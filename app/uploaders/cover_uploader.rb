f5d3206f06059a62932234a03b17c5936305deab
Ammar Shah <ammarshah123@gmail.com>
2018-01-26 17:00:21 +0500

    New default image for user and company profile cover photo

--- app/uploaders/cover_uploader.rb
+++ app/uploaders/cover_uploader.rb
@@ -17,7 +17,7 @@ class CoverUploader < CarrierWave::Uploader::Base
   # Provide a default URL as a default if there hasn't been a file uploaded:
   def default_url(*args)
     # For Rails 3.1+ asset pipeline compatibility:
-    ActionController::Base.helpers.asset_path("fallback/cover/" + [version_name, "default.jpg"].compact.join('_'))
+    ActionController::Base.helpers.asset_path("fallback/user/cover/" + [version_name, "default.jpg"].compact.join('_'))
   
     # "/images/fallback/" + [version_name, "default.png"].compact.join('_')
   end
