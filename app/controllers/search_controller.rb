45e2e062684d6dc5d1f3bdd47b50d280c2f8591b
Ammar Shah <ammarshah123@gmail.com>
2018-03-09 15:47:52 +0500

    Add an ascending sort order as a default on search results

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -4,9 +4,9 @@ class SearchController < ApplicationController
     respond_to do |format|
       if @search.valid?
         if user_signed_in?
-          @routes = Route.get_matches(search_params, current_user)
+          @routes = Route.get_matches(search_params, current_user).order(price: :asc)
         else
-          @routes = Route.get_matches(search_params, current_user).limit(5)
+          @routes = Route.get_matches(search_params, current_user).limit(5).order(price: :asc)
         end
         Search.create_paper_trail_version(search_params) # Manually create a paper trail version to track searches
         format.html
